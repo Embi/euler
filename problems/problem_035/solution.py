7e8f6d6d031edd5978dfeb65b17a80539ad33f5f
Embi <git@ember.sk>
2025-02-24 16:21:00 +0100

    Add solution to problem 35

--- problems/problem_035/solution.py
+++ problems/problem_035/solution.py
@@ -1,8 +1,9 @@
 import json
 import time
 import math
+from typing import Set, Iterator
 
-def get_primes(n):
+def get_primes(n: int) -> Set[int]:
     numbers = list(range(n))
     # Bound for primality check
     max_check = int(math.sqrt(n)) + 1
@@ -14,34 +15,39 @@ def get_primes(n):
             except IndexError:
                 pass
         idx += 1
-
     numbers = set(numbers)
-    numbers.remove(0)
     numbers.remove(-1)
-    return list(numbers)
+    numbers.remove(0)
+    numbers.remove(1)
+    return numbers
 
-def rotations(number):
+def rotations(number: int) -> Iterator[int]:
     rotation = str(number)
     for i in range(len(rotation)):
         rotation = rotation[1:] + rotation[:1]
         yield int(rotation)
 
-def circular_primes(primes):
-    _circular_primes = []
-    primes = list(filter(lambda x: '0' not in str(x), primes))
+def circular_primes(below_n: int) -> Set[int]:
+    _circular_primes = set()
+    primes = get_primes(below_n)
+    # A prime that contains 0 can not be circular because at lease one rotation
+    # will be evenly divisible by 10
+    primes = set(filter(lambda x: '0' not in str(x), primes))
     for prime in primes:
+        if prime in _circular_primes:
+            continue
         _rotations = list(rotations(prime))
         try:
             for rotation in _rotations:
-                primes.remove(rotation)
-        except ValueError:
+                assert rotation in primes
+        except AssertionError:
             continue
-        _circular_primes.extend(_rotations)
+        _circular_primes.update(_rotations)
     return _circular_primes
 
 
 def solution() -> int:
-    pass
+    return len(circular_primes(1000000))
 
 cpu_s, wall_s = time.process_time(), time.time()
 result = solution()
