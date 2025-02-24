33569c90cffd556c8be38d3e478e0629086c458e
Embi <git@ember.sk>
2025-02-24 16:21:30 +0100

    Add solution to problem 69

--- problems/problem_069/solution.py
+++ problems/problem_069/solution.py
@@ -17,40 +17,16 @@ def get_primes(up_to: int) -> List[int]:
     primes.remove(0)
     return sorted(primes)
 
-_PRIMES = get_primes(1000000)
-_PRIMES_LOOKUP = set(_PRIMES)
-
-def get_prime_factors(n):
-    prime_factors = set()
-    limit = int(n**0.5) + 1
-    for prime in _PRIMES:
-        if prime == max(prime, min(limit, n)):
-            break
-        while n%prime == 0:
-            prime_factors.add(prime)
-            n = n // prime
-    if n in _PRIMES_LOOKUP:
-        prime_factors.add(n)
-    return prime_factors
-
-def get_totient(n):
-    factors = get_prime_factors(n)
-    non_relative_primes = set()
-    for factor in factors:
-        non_relative_primes.update(range(factor, n, factor))
-    return (n - len(non_relative_primes) - 1)
+_PRIMES = get_primes(1000)
 
 def solution() -> int:
-    x = []
-    max_t = 0
-    max_t_n = 0
-    i = 2
-    with Pool(10) as p:
-        totients = list(p.imap(get_totient, range(200000, 300000), 10000))
-        for totient in totients:
-            max_t, max_t_n = max((max_t, max_t_n), (i/totient, i))
-            i += 1
-    return max_t_n
+    # We are essentially just looking for a number smaller or equal to
+    # 1000000 with most prime factors
+    result = 1
+    for p in _PRIMES:
+        if result * p > 1000000:
+            return result
+        result = result * p
 
 cpu_s, wall_s = time.process_time(), time.time()
 result = solution()
